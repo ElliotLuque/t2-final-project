427b7c2da5d3b573c049ef035037477ff901ec7c
ElliotLuque <elliotluque@gmail.com>
2022-12-17 19:06:26 +0100

    feat: add animations

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById("modal")
 
 modal.addEventListener("click", () => {
+    const image = document.querySelector(".modal__content img")
+    image.style.opacity = 0
     modal.style.display = "none"
 })
 
@@ -9,9 +11,29 @@ const images = document.querySelectorAll(".masonry div img")
 images.forEach(image => {
     image.addEventListener("click", () => {
         modal.style.display = "flex"
+        modal.style.opacity = 1
 
         const container = document.querySelector(".modal__content")
         container.innerHTML = `<img src="${image.src}" />`
         container.style.display = "flex"
+    })
+})
+
+const filterHeaderButtons = document.querySelectorAll(".filter__header > svg")
+
+filterHeaderButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+
+        button.style.rotate = "180deg" === button.style.rotate 
+            ? "0deg" 
+            : "180deg";
+
+        const filter = button.parentElement.parentElement
+        const filterBody = filter.children[1]
+
+        index === 2 
+            ? filterBody.style.display = "none" === filterBody.style.display ? "flex" : "none"
+            : filterBody.style.display = "none" === filterBody.style.display ? "block" : "none"
+
     })
 })
